9257f225c629b0f181fbb439da7ed8abe0064b04
4okoboi <zakiroffkam@gmail.com>
2025-07-02 00:16:13 +0300

    asdfasdf

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 CORS_ALLOWED_ORIGINS = [
     'http://89.208.113.95:80',
