33da844d7a9526ddd66581f592278f1fd579739e
4okoboi <zakiroffkam@gmail.com>
2025-07-02 00:30:47 +0300

    serial

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -40,7 +40,7 @@ class RecipeListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = (
-            'id', 'author', 'ingredients', 'is_favorited', 'is_in_shopping_cart',
+            'id', 'author', 'ingredients', 'is_favourite', 'is_in_shopping_cart',
             'name', 'image', 'text', 'cooking_time'
         )
 
