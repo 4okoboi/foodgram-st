79f2153dad9dbbe02eca7a00e6d2762e4890f421
4okoboi <zakiroffkam@gmail.com>
2025-07-02 00:04:12 +0300

    allowed hosts

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-e%tdg@@@w&n&^ohfldkm1x8osdhvqp4eog9bknae$w9cbk8jw8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
